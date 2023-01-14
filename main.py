167b913ba32bc8aa43031cc3841bfb626eaa482d
Tupi Guedes <tupiguedes@outlook.com>
2023-01-14 00:46:46 -0300

    Testando audio com a biblioteca vosk

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import speech_recognition as sr
+# Gravar o audio online
+# import speech_recognition as sr
 
-r = sr.Recognizer()
+# r = sr.Recognizer()
 
-# Abrir o microfone e capturar a voz
-with sr.Microphone() as fala:
-    while True:
-        try:
-            print('Microfone ligado! \nDiga alguma coisa, amigo!')
-            # Grava a fala
-            audio = r.listen(fala)
-            # Traduz o que foi gravado e mostra em tempo real
-            print('Você disse: ', r.recognize_google(audio, language='pt'))
-        except sr.UnknownValueError:
-            pass
-        except sr.RequestError:
-            pass
+# # Abrir o microfone e capturar a voz
+# with sr.Microphone() as fala:
+#     while True:
+#         try:
+#             print('Microfone ligado! \nDiga alguma coisa, amigo!')
+#             # Grava a fala
+#             audio = r.listen(fala)
+#             # Traduz o que foi gravado e mostra em tempo real
+#             print('Você disse: ', r.recognize_google(audio, language='pt'))
+#         except sr.UnknownValueError:
+#             pass
+#         except sr.RequestError:
+#             pass
+
+# Offline
